94c98484200201330a6940cfb454ac94c2ecb6a8
MSI\Tanay <tanaykulkarni7@gmail.com>
2020-10-11 16:11:21 +0530

    added port changes

--- Utils/mailer.js
+++ Utils/mailer.js
@@ -3,8 +3,8 @@ const mailer = require("nodemailer");
 
 const transporter = mailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD,
